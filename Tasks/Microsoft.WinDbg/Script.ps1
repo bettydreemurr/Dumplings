78afcab517a6be0b9b1b7797a670fbf4cf8ac4ea
‌ <46537034+Exorcism0666@users.noreply.github.com>
2025-01-12 22:18:10 +0100

    idkk

--- Tasks/Microsoft.WinDbg/Script.ps1
+++ Tasks/Microsoft.WinDbg/Script.ps1
@@ -1,33 +1,38 @@
-$Object1 = Invoke-RestMethod -Uri 'https://windbg.download.prss.microsoft.com/dbazure/prod/1-0-0/windbg.appinstaller'
+$response = Invoke-WebRequest -Uri 'https://windbg.download.prss.microsoft.com/dbazure/prod/1-0-0/windbg.appinstaller'
+$xmlContent = [xml]$response.Content
 
-# Version
-$this.CurrentState.Version = $Object1.AppInstaller.MainBundle.Version
+# Gérer l'espace de noms
+$namespaceManager = New-Object System.Xml.XmlNamespaceManager($xmlContent.NameTable)
+$namespaceManager.AddNamespace('appx', 'http://schemas.microsoft.com/appx/appinstaller/2018')
 
-# Installer
+# Accéder aux nœuds
+$appInstallerNode = $xmlContent.SelectSingleNode('//appx:AppInstaller', $namespaceManager)
+$mainBundleNode = $appInstallerNode.SelectSingleNode('appx:MainBundle', $namespaceManager)
+
+# Utiliser les données
+$this.CurrentState.Version = $mainBundleNode.Version
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x86'
-  InstallerUrl = $Object1.AppInstaller.MainBundle.Uri
+    Architecture = 'x86'
+    InstallerUrl = $mainBundleNode.Uri
 }
-# Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'x64'
-  InstallerUrl = $Object1.AppInstaller.MainBundle.Uri
+    Architecture = 'x64'
+    InstallerUrl = $mainBundleNode.Uri
 }
-# Installer
 $this.CurrentState.Installer += [ordered]@{
-  Architecture = 'arm64'
-  InstallerUrl = $Object1.AppInstaller.MainBundle.Uri
+    Architecture = 'arm64'
+    InstallerUrl = $mainBundleNode.Uri
 }
 
 switch -Regex ($this.Check()) {
-  'New|Changed|Updated' {
-    $this.Print()
-    $this.Write()
-  }
-  'Changed|Updated' {
-    $this.Message()
-  }
-  'Updated' {
-    $this.Submit()
-  }
+    'New|Changed|Updated' {
+        $this.Print()
+        $this.Write()
+    }
+    'Changed|Updated' {
+        $this.Message()
+    }
+    'Updated' {
+        $this.Submit()
+    }
 }
