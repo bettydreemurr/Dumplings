de1bb2680d0870cf8db16bd6ab2321e3269ff3c9
‌ <46537034+Exorcism0666@users.noreply.github.com>
2025-01-13 23:13:31 +0100

    Fixed

--- Tasks/prefix-dev.pixi/Script.ps1
+++ Tasks/prefix-dev.pixi/Script.ps1
@@ -9,8 +9,13 @@ $this.CurrentState.Version = $Object1.tag_name -creplace '^v'
 # Installer
 $this.CurrentState.Installer += [ordered]@{
   Architecture = 'x64'
-  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.msi') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.msi') -and $_.name.Contains('x86_64') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
 }
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.msi') -and !($_.name.Contains('x86_64')) }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
 
 # ReleaseTime
 $this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
