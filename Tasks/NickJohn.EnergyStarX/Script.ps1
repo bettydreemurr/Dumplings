4963f57b759c623a7455ad3b13a9f10fa05f02e9
‌ <46537034+Exorcism0666@users.noreply.github.com>
2025-01-18 10:21:28 +0100

    Add NickJohn.EnergyStarX

---
+++ Tasks/NickJohn.EnergyStarX/Script.ps1
@@ -0,0 +1,68 @@
+$RepoOwner = 'files-community'
+$RepoName = 'Files'
+
+$Object1 = Invoke-GitHubApi -Uri "https://api.github.com/repos/${RepoOwner}/${RepoName}/releases/latest"
+
+
+# Fonction pour convertir une version au format X.X.X.X
+function Convert-ToFourSegmentVersion {
+    param (
+        [string]$Version
+    )
+    # Séparer la version en segments
+    $segments = $Version -split '\.'
+    # Compléter avec des zéros pour atteindre 4 segments
+    while ($segments.Count -lt 4) {
+        $segments += '0'
+    }
+    # Rejoindre les segments avec des points
+    return $segments -join '.'
+}
+
+# Version
+$rawVersion = $Object1.tag_name -creplace '^v'
+$this.CurrentState.Version = Convert-ToFourSegmentVersion $rawVersion
+
+
+# Installer
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'x64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.Msix') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+$this.CurrentState.Installer += [ordered]@{
+  Architecture = 'arm64'
+  InstallerUrl = $Object1.assets.Where({ $_.name.EndsWith('.Msix') }, 'First')[0].browser_download_url | ConvertTo-UnescapedUri
+}
+
+# ReleaseTime
+$this.CurrentState.ReleaseTime = $Object1.published_at.ToUniversalTime()
+
+if (-not [string]::IsNullOrWhiteSpace($Object1.body)) {
+  # ReleaseNotes (en-US)
+  $this.CurrentState.Locale += [ordered]@{
+    Locale = 'en-US'
+    Key    = 'ReleaseNotes'
+    Value  = ($Object1.body | ConvertFrom-Markdown).Html | ConvertFrom-Html | Get-TextContent | Format-Text
+  }
+} else {
+  $this.Log("No ReleaseNotes (en-US) for version $($this.CurrentState.Version)", 'Warning')
+}
+
+# ReleaseNotesUrl
+$this.CurrentState.Locale += [ordered]@{
+  Key   = 'ReleaseNotesUrl'
+  Value = $Object1.html_url
+}
+
+switch -Regex ($this.Check()) {
+  'New|Changed|Updated' {
+    $this.Write()
+  }
+  'Changed|Updated' {
+    $this.Print()
+    $this.Message()
+  }
+  'Updated' {
+    $this.Submit()
+  }
+}
